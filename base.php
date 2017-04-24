4dc70553ad2cef97c9c8a25d9a7dd518c3997eec
MitchReyno <mitchreyno1@gmail.com>
2017-04-24 17:39:10 +1000

    Quick template

---
+++ base.php
@@ -0,0 +1,36 @@
+<?php require_once 'ti.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<title>People Health Pharmacy -
+		<?php startblock('title') ?><?php endblock() ?>
+	</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,
+								   initial-scale=1.0" />
+	<link href="css/base.css" rel="stylesheet" />
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5
+	  elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page
+		via file:// -->
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	</head>
+	<body class = "container">
+		<header>
+			<img id="banner" src="http://www.nationalcollegeofpharmacy.org/images/banner.jpg" alt="People Health Pharmacy"/>
+			<h1>People Health Pharmacy</h1>
+			<?php include "menu.php"; ?>
+		</header>
+
+
+		<!-- jQuery - required for Bootstrap's JavaScript plugins) -->
+		<script src="js/jquery.min.js"></script>
+		<!-- All Bootstrap plug-ins file -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
--- menu.php
+++ menu.php
@@ -1,35 +1,33 @@
 
-<div  class="collapse navbar-collapse" id="the-navbar-to-collapse">
   <ul class="nav nav-pills">
-    <li class="active"><a href="#">Home</a></li>
-    <li><a href="#">Add Sales</a></li>
-    <li><a href="#">Search</a></li>
-    <li><a href="#">Transition</a></li>
-    <li class="dropdown">
-      <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-            Reports <span class="caret"></span>
-      </a>
-      <ul class="dropdown-menu">
-        <li><a href="#">Monthly</a></li>
-        <li><a href="#">Weekly</a></li>
-        <li><a href="#">Annualy</a></li>
-      </ul>
-    </li>
+	<li class="active"><a href="#">Home</a></li>
+	<li><a href="#">Add Sales</a></li>
+	<li><a href="#">Search</a></li>
+	<li><a href="#">Transition</a></li>
+	<li class="dropdown">
+	  <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+			Reports <span class="caret"></span>
+	  </a>
+	  <ul class="dropdown-menu">
+		<li><a href="#">Monthly</a></li>
+		<li><a href="#">Weekly</a></li>
+		<li><a href="#">Annualy</a></li>
+	  </ul>
+	</li>
 
-    <li class="dropdown">
-     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-            Hi(<?php $username ?>) <span class="caret"></span>
-      </a>
+	<li class="dropdown">
+	 <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+			Hi(<?php $username ?>) <span class="caret"></span>
+	  </a>
 
-      <ul class="dropdown-menu">
-        <li><a href="#">Edit Profile</a></li>
-        <li class="divider"></li>
-        <li><a href="#">Log Out</a></li>
-      </ul>
-    </li>
+	  <ul class="dropdown-menu">
+		<li><a href="#">Edit Profile</a></li>
+		<li class="divider"></li>
+		<li><a href="#">Log Out</a></li>
+	  </ul>
+	</li>
 
   </ul>
-</div>
 
 <!-- <li class="dropdown">
 <a class="dropdown-toggle" data-toggle="dropdown" href="#">
@@ -42,9 +40,3 @@ Settings <span class="caret"></span>
 <li><a href="#">About</a></li>
 </ul>
 </li>-->
-
-<!-- jQuery - required for Bootstrap's JavaScript plugins) -->
-<script src="js/jquery.min.js"></script>
-<!-- All Bootstrap plug-ins file -->
-<script src="js/bootstrap.min.js"></script>
-
