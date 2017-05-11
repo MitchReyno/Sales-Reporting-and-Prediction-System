dd19bb37f1639a8cca80f2a7f1ee885e0609f5b2
MitchReyno <mitchreyno1@gmail.com>
2017-05-11 23:17:43 +1000

    Finished reports

--- reports/week.php
+++ reports/week.php
@@ -5,7 +5,7 @@
 	}
 	else
 	{
-		$dates = array("Monday"=>date('d/m/Y',strtotime("monday this week", strtotime($_POST['wdate']))),"Tuesday"=>date('d/m/Y',strtotime("tuesday this week", strtotime($_POST['wdate']))),"Wednesday"=>date('d/m/Y',strtotime("wednesday this week", strtotime($_POST['wdate']))),"Thursday"=>date('d/m/Y',strtotime("thursday this week", strtotime($_POST['wdate']))),"Friday"=>date('d/m/Y',strtotime("friday this week", strtotime($_POST['wdate']))),"Saturday"=>date('d/m/Y',strtotime("saturday this week", strtotime($_POST['wdate']))),"Sunday"=>date('d/m/Y',strtotime("sunday this week", strtotime($_POST['wdate']))));
+		$dates = array("Monday"=>date('d/m/Y',strtotime("monday this week", strtotime($_POST['wdate']))),"Tuesday"=>date('d/m/Y',strtotime("monday this week +1 day", strtotime($_POST['wdate']))),"Wednesday"=>date('d/m/Y',strtotime("monday this week +2 day", strtotime($_POST['wdate']))),"Thursday"=>date('d/m/Y',strtotime("monday this week +3 day", strtotime($_POST['wdate']))),"Friday"=>date('d/m/Y',strtotime("monday this week +4 day", strtotime($_POST['wdate']))),"Saturday"=>date('d/m/Y',strtotime("monday this week +5 day", strtotime($_POST['wdate']))),"Sunday"=>date('d/m/Y',strtotime("monday this week +6 day", strtotime($_POST['wdate']))));
 		echo '<p>'.$_POST['wdate'].'</p>';
 	}
  ?>
@@ -29,27 +29,37 @@
 		<?php
 			$query = "SELECT orders.order_date AS date, SUM(products.unit_price * order_details.Quantity - order_details.Discount) AS totalsales FROM orders INNER JOIN order_details ON order_details.Order_ID = orders.Order_ID INNER JOIN products ON order_details.product_id = products.product_id
 WHERE YEARWEEK(orders.order_date, 1) = YEARWEEK(STR_TO_DATE('".$dates["Monday"]."', '%d/%m/%Y'), 1) GROUP BY orders.order_date, YEARWEEK(orders.order_date, 1) ORDER BY orders.order_date;";
+			$data = array();
 			$result = mysqli_query($conn, $query);
-			$table = array();
 			while ($row = @mysqli_fetch_assoc($result)){
-				$table[] = $row;
+				$data += [$row['date'] => $row['totalsales']];
 			}
 			$weektotal = 0;
 			foreach ($dates as $d => $d_value)
 			{
+				$formatteddate = DateTime::createFromFormat('d/m/Y', $d_value)->format('Y-m-d');
+				$formatteddate = (string)$formatteddate;
 				$daytotal = 0;
-				foreach($table as $tablerow)
+				if(array_key_exists($formatteddate, $data))
 				{
-					if(date('d/m/Y', strtotime($tablerow['date'])) == $d_value)
-					{
-						$daytotal = $tablerow['totalsales'];
-						break;
-					}
+					$daytotal = $data[$formatteddate];
 				}
-				echo '<td>$'.$daytotal.'</td>';
+				echo '<td>';
+				if($daytotal == 0)
+				{
+					echo "-";
+				}
+				else
+				{
+					echo "$".number_format($daytotal, 2);
+				}
+				echo '</td>';
 				$weektotal = $weektotal + $daytotal;
 			}
 		echo '<td>$'.$weektotal.'</td>';
 		?>
 	</tr>
 </table>
+<?php
+	echo '<h3>Total weekly sales: $'.number_format($weektotal, 2).'</h3>';
+?>
---
+++ reports/year.php
@@ -0,0 +1,55 @@
+<?php
+	if(!isset($_POST['yyear']))
+	{
+		$year = date('Y');
+	}
+	else
+	{
+		$year = $_POST['yyear'];
+	}
+ ?>
+<form action="./reports.php#Annual" method="post">
+	<h3>Enter year to display report:</h3>
+	<select name="yyear">
+		<?php
+			foreach (range(date('Y'), 1950) as $value) {
+					echo "<option value=\"".$value."\"";
+					if ($value == $year){echo " selected";}
+					echo ">" . $value . "</option> ";
+			}
+		?>
+	</select>
+	<input type="submit" value="Submit"/>
+</form>
+<h3>Annual report for <em><strong><?php echo $year; ?></strong></em></h3>
+<table class='table table-striped table-hover'>
+	<?php
+	$query = "SELECT MONTH(orders.order_date) AS date, SUM(products.unit_price * order_details.Quantity - order_details.Discount) AS totalsales FROM orders INNER JOIN order_details ON order_details.Order_ID = orders.Order_ID INNER JOIN products ON order_details.product_id = products.product_id
+WHERE YEAR(orders.order_date) = ".$year." GROUP BY MONTH(orders.order_date) ORDER BY orders.order_date;";
+	$result = mysqli_query($conn, $query);
+	$monthsales = array();
+
+	while($row = @mysqli_fetch_assoc($result))
+	{
+		$monthsales += [$row['date'] => $row['totalsales']];
+	}
+	$annualsales = 0;
+	for($i = 1; $i <= 12; $i++)
+	{
+		echo '<tr><td>';
+		echo date('F', mktime(0, 0, 0, $i, 1, $year));
+		echo '</td><td>';
+		if(array_key_exists($i, $monthsales)){
+			echo "$".number_format($monthsales[$i], 2);
+			$annualsales += $monthsales[$i];
+		} else
+		{
+			echo "-";
+		}
+		echo '</td></tr>';
+	}
+	?>
+</table>
+<?php
+	echo '<h3>Total annual sales: $'.number_format($annualsales, 2).'</h3>';
+?>
